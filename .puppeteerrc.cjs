e93d1f54bc9589c52949865c8dd29535be58917c
Vinicios Caleiras <123804460+viniaprendiz@users.noreply.github.com>
2026-07-24 11:47:56 -0300

    Add Puppeteer configuration for Chromium browser path

---
+++ .puppeteerrc.cjs
@@ -0,0 +1,6 @@
+module.exports = {
+  cacheDirectory: './.cache/puppeteer',
+      downloadHost: 'https://registry.npmmirror.com/-/binary/chrome',
+      skipChromiumDownload: true,
+      executablePath: '/usr/bin/chromium-browser' || '/usr/bin/chromium' || '/snap/bin/chromium'
+    };
